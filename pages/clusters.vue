486730f2ff3c3f960f23837c80bffa0508f82ca1
Zach Dyer <zach.dyer@gmail.com>
2020-08-20 11:54:42 -0500

    Update clusters.vue

--- pages/clusters.vue
+++ pages/clusters.vue
@@ -10,6 +10,17 @@
         <b-input id="inline-form-input-username" placeholder="Cluster Cooridant Y" v-model="y"></b-input>
       </b-input-group>
       <p>Star Count: {{starCount }}</p>
+      <b-card
+        v-for="(star, index) in stars"
+        v-bind:data="star"
+        v-bind:key="index"
+        :img-src="star.image"
+        img-alt="Card image"
+        img-left
+        class="mb-3"
+      >
+        <b-card-text>Star ID: {{index}} X: {{star.x}} Y: {{star.y}}</b-card-text>
+      </b-card>
       <Nav />
     </div>
   </div>
@@ -19,9 +30,24 @@
 Math.range = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
-function Star(x, y) {
-  this.x = x;
-  this.y = y;
+Math.pick = function (list) {
+  return list[Math.range(0, list.length)];
+};
+function Star(starID, clusterX, clusterY, clusterSize) {
+  this.seed = `?clusterX=${clusterX}&clusterY=${clusterY}&starID=${starID}`;
+  Math.seedrandom(this.id);
+  this.id = starID;
+  this.x = Math.range(0, clusterSize);
+  this.y = Math.range(0, clusterSize);
+  let images = [
+    "https://cdn.mos.cms.futurecdn.net/Pkx5h33mQDLDbYrCPv62rC.jpg",
+    "https://www.rnz.co.nz/assets/news/219110/eight_col_beetle.jpg?1577494395",
+    "https://earthsky.org/upl/2020/02/Castor-by-Espenak.jpg",
+    "https://www.siliconrepublic.com/wp-content/uploads/2017/08/Star-close-up-718x523.jpg",
+    "https://i.pinimg.com/originals/bb/75/25/bb752540154c00faf633c200d188c102.jpg",
+    "https://www.factsjustforkids.com/images/red-giant-star.png",
+  ];
+  this.image = Math.pick(images);
 }
 function Cluster(x, y) {
   this.x = x;
@@ -31,12 +57,10 @@ function Cluster(x, y) {
   this.seed = x + y;
   // generate number of stars based on a seeded range.
   Math.seedrandom(`Cluster x:${x}, y:${y}`);
-  this.starCount = Math.range(2, 12);
+  this.starCount = Math.range(2, 5);
   // generate a list of stars based on the star count
-  for (var i = 0; i < this.starCount; i++) {
-    this.stars.push(
-      new Star(Math.range(0, this.size), Math.range(0, this.size))
-    );
+  for (var starID = 0; starID < this.starCount; starID++) {
+    this.stars.push(new Star(starID, this.x, this.y, this.size));
   }
 }
 export default {
@@ -63,12 +87,14 @@ export default {
       if (this.x) {
         var cluster = new Cluster(this.x, this.y);
         this.starCount = cluster.starCount;
+        this.stars = cluster.stars;
       }
     },
     y() {
       if (this.y) {
         var cluster = new Cluster(this.x, this.y);
         this.starCount = cluster.starCount;
+        this.stars = cluster.stars;
       }
     },
   },
@@ -105,5 +131,8 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+.card-img-left {
+  height: 100px;
 }
 </style>
